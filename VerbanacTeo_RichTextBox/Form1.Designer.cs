5f9094724851248245838e72afb73a30fb852459
teoVerbanac <44996650+teoVerbanac@users.noreply.github.com>
2019-01-22 21:25:25 +0100

    Add files via upload

---
+++ VerbanacTeo_RichTextBox/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace VerbanacTeo_RichTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCitaj = new System.Windows.Forms.Button();
+            this.btnBrisi = new System.Windows.Forms.Button();
+            this.RichTxtBoxMain = new System.Windows.Forms.RichTextBox();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oProgramuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCitaj
+            // 
+            this.btnCitaj.Location = new System.Drawing.Point(173, 68);
+            this.btnCitaj.Name = "btnCitaj";
+            this.btnCitaj.Size = new System.Drawing.Size(75, 23);
+            this.btnCitaj.TabIndex = 0;
+            this.btnCitaj.Text = "Citaj";
+            this.btnCitaj.UseVisualStyleBackColor = true;
+            this.btnCitaj.Click += new System.EventHandler(this.btnCitaj_Click);
+            // 
+            // btnBrisi
+            // 
+            this.btnBrisi.Location = new System.Drawing.Point(355, 68);
+            this.btnBrisi.Name = "btnBrisi";
+            this.btnBrisi.Size = new System.Drawing.Size(75, 23);
+            this.btnBrisi.TabIndex = 1;
+            this.btnBrisi.Text = "Brisi";
+            this.btnBrisi.UseVisualStyleBackColor = true;
+            this.btnBrisi.Click += new System.EventHandler(this.btnBrisi_Click);
+            // 
+            // RichTxtBoxMain
+            // 
+            this.RichTxtBoxMain.Location = new System.Drawing.Point(173, 146);
+            this.RichTxtBoxMain.Name = "RichTxtBoxMain";
+            this.RichTxtBoxMain.Size = new System.Drawing.Size(257, 189);
+            this.RichTxtBoxMain.TabIndex = 2;
+            this.RichTxtBoxMain.Text = "";
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(173, 381);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(257, 40);
+            this.btnSpremi.TabIndex = 3;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            this.btnSpremi.Click += new System.EventHandler(this.btnSpremi_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oProgramuToolStripMenuItem
+            // 
+            this.oProgramuToolStripMenuItem.Name = "oProgramuToolStripMenuItem";
+            this.oProgramuToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.oProgramuToolStripMenuItem.Text = "O programu";
+            this.oProgramuToolStripMenuItem.Click += new System.EventHandler(this.oProgramuToolStripMenuItem_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::VerbanacTeo_RichTextBox.Properties.Resources.cc_licence4;
+            this.pictureBox1.Location = new System.Drawing.Point(505, 371);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(88, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.RichTxtBoxMain);
+            this.Controls.Add(this.btnBrisi);
+            this.Controls.Add(this.btnCitaj);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = " ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCitaj;
+        private System.Windows.Forms.Button btnBrisi;
+        private System.Windows.Forms.RichTextBox RichTxtBoxMain;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oProgramuToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ VerbanacTeo_RichTextBox/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerbanacTeo_RichTextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCitaj_Click(object sender, EventArgs e)
+        {
+            // poziv funkcije LoadFile() za učitavanje tekst dokumenta
+            // u richTextBox
+            RichTxtBoxMain.LoadFile(@"C:Tempproba.rtf");
+
+        }
+
+        private void btnBrisi_Click(object sender, EventArgs e)
+        {
+            // poziv funkije Clear() za brisanje sadržaja richTextBox-a
+            RichTxtBoxMain.Clear();
+        }
+
+        private void btnSpremi_Click(object sender, EventArgs e)
+        {
+            // poziv funkcije SaveFile() za spremanje sadržaja richTextBox-a
+            // u dokumentu, uz navodjenje točno određenog mjesta
+            RichTxtBoxMain.SaveFile(@"C:Tempproba.rtf");
+            MessageBox.Show("Tekst spremljen");
+        }
+        
+
+        private void oProgramuToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Naziv programa: Timer komponenta \nIme učenika: Teo \nPrezime učenika: Verbanac \nDatum izrade: 22.1.2019\nDavatelj licence dopušta umnožavanje, distribuiranje i priopćavanje djela javnosti. Zauzvrat primatelji licence moraju imenovanjem priznati i označiti izvornog autora.");
+
+        }
+    }
+}
+
