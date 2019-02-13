3ed6e7e16ef22efba24bf5f51717a4762aa7e504
teoVerbanac <44996650+teoVerbanac@users.noreply.github.com>
2019-02-13 12:19:39 +0100

    Add files via upload

---
+++ klaseIObjekti/klaseAndObjekti/Klase/Vozilo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klaseAndObjekti.Klase
+{
+    class Vozilo
+    {
+        int brojKotaca;
+        bool leti, pliva, vozi;
+        string vrsta;
+        string oznaka;
+
+        public void setBrojKotaca(int brojKotaca)
+        {
+            this.brojKotaca = brojKotaca;
+        }
+
+        public int getBrojKotaca()
+        {
+            return this.brojKotaca;
+        }
+
+        public void setLeti(bool leti)
+        {
+            this.leti = leti;
+        }
+
+        public bool getLeti()
+        {
+            return this.leti;
+        }
+
+        public void setPliva(bool pliva)
+        {
+            this.pliva = pliva;
+        }
+
+        public bool getPliva()
+        {
+            return this.pliva;
+        }
+
+        public void setVozi(bool vozi)
+        {
+            this.vozi = vozi;
+        }
+
+        public bool getVozi()
+        {
+            return this.vozi;
+        }
+
+        public void setVrsta(string vrsta)
+        {
+            this.vrsta = vrsta;
+        }
+
+        public string getVrsta()
+        {
+            return this.vrsta;
+        }
+
+        public void setOznaka(string oznaka)
+        {
+            this.oznaka = oznaka;
+        }
+
+        public string getOznaka()
+        {
+            return this.oznaka;
+        }
+
+        public Vozilo(int brojKotaca, bool leti, bool pliva, bool vozi, string vrsta, string oznaka)
+        {
+            this.brojKotaca = brojKotaca;
+            this.leti = leti;
+            this.pliva = pliva;
+            this.vozi = vozi;
+            this.vrsta = vrsta;
+            this.oznaka = oznaka;
+        }
+
+        public Vozilo()
+        {
+
+        }
+
+        public string ispis()
+        {
+            return "Vrsta: " + vrsta + "\nOznaka: " + oznaka + "\nPliva: " + pliva +
+                            "\nLeti: " + leti + "\nVozi: " + vozi + "\nBroj kotača: " + brojKotaca;
+
+        }
+        public override string ToString()
+        {
+            return "Vrsta: " + vrsta + "\nOznaka: " + oznaka + "\nPliva: " + pliva +
+                "\nLeti: " + leti + "\nVozi: " + vozi + "\nBroj kotača: " + brojKotaca;
+        }
+
+    }
+}
---
+++ klaseIObjekti/klaseAndObjekti/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using klaseAndObjekti.Klase;
+
+namespace klaseAndObjekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+            Vozilo ime = new Vozilo();
+            bool jos = true;
+            
+            do
+            {
+                Console.WriteLine("Kojoj vrsti pripada:");
+                ime.setVrsta(Console.ReadLine().ToString());
+                Console.WriteLine("Broj kotača(ako nema napšite 0)");
+                ime.setBrojKotaca(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("Kreće se po zraku(true/false):");
+                ime.setLeti(bool.Parse(Console.ReadLine()));
+                Console.WriteLine("Kreće se po vodi(true/false):");
+                ime.setPliva(bool.Parse(Console.ReadLine()));
+                Console.WriteLine("Kreće se po kopnu(true/false):");
+                ime.setVozi(bool.Parse(Console.ReadLine()));
+                Console.WriteLine("Registracija");
+                ime.setOznaka(Console.ReadLine().ToString());
+
+                Console.WriteLine(ime.ispis());
+
+                Console.WriteLine("Želite li upisati još neko vozilo?(true/false)");
+                jos = bool.Parse(Console.ReadLine());
+            } while (jos == true);
+
+            /*
+            Vozilo kamijon = new Vozilo(8, false, false, true, "TAM", "RI1987AR");
+            Console.WriteLine("Vrsta: " + ime.getVrsta() + " Oznaka: " +
+                ime.getOznaka() + "\nLeti: " + ime.getLeti() + "\nPlovi: "+ ime.getPliva()
+                + "\nVozi: " + ime.getVozi() + "\nBroj kotača: "+ime.getBrojKotaca() + "\n");
+            
+            Console.WriteLine(kamijon.ToString());
+            */
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
+    
+
